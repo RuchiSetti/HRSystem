13fbe09aee087b3c725fb382eb187818a8c76f6f
enter username <ruchitasetti@gmail.com>
2025-11-28 02:38:02 +0530

    Fix App import case issue

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
+
+
 // Components
 import HomePage from './components/Pages/HomePage';
 import Login from './components/Auth/Login';
